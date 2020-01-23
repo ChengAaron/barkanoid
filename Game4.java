8c0cab17efa0970c82b352ce24e7fdbe928b6c45
ChengAaron <48865696+ChengAaron@users.noreply.github.com>
2020-01-23 11:33:59 -0500

    Update Game4.java

--- Game4.java
+++ Game4.java
@@ -8,7 +8,7 @@ public class Game4 extends JFrame implements ActionListener{
 	GamePanel game;
 
     public Game4() {
-		super("Move the Box");
+		super("Move the Ball");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//setSize(800,650);
 
@@ -37,7 +37,7 @@ public class Game4 extends JFrame implements ActionListener{
 }
 
 class GamePanel extends JPanel implements KeyListener{
-	private int boxx,boxy,moveH,moveV,angle,vx,vy;
+	private int boxx,boxy,moveH,moveV,vx,vy;
 	private boolean []keys;
 	private Image back;
 	private Game4 mainFrame;
@@ -45,19 +45,19 @@ class GamePanel extends JPanel implements KeyListener{
 
 	public GamePanel(Game4 m){
 		keys = new boolean[KeyEvent.KEY_LAST+1];
-		back = new ImageIcon("OuterSpace.jpg").getImage();
+		back = new ImageIcon("background.png").getImage();
 
 		mainFrame = m;
 	    boxx = 170;
         boxy = 170;
-        vx = 5;
-        vy = 1;
+        vx = 5; //horizontal movement of ball
+        vy = 2; //the angle of the ball
         moveH = vx;
         moveV = vy;
         moving = false;
 
 
-		setPreferredSize( new Dimension(800, 600));
+		setPreferredSize( new Dimension(550, 650));
         addKeyListener(this);
 	}
 
@@ -69,15 +69,6 @@ class GamePanel extends JPanel implements KeyListener{
     }
 
 	public void move(){
-		/*if(keys[KeyEvent.VK_RIGHT] ){
-			boxx += speed;
-		}
-		if(keys[KeyEvent.VK_LEFT] ){
-			boxx -= speed;
-		}
-		if(keys[KeyEvent.VK_DOWN] ){
-			boxy += speed;
-		}*/
 		if(keys[KeyEvent.VK_UP] ){
 			moving = true;
 		}
@@ -111,40 +102,84 @@ class GamePanel extends JPanel implements KeyListener{
     }
 
     public void collisions() {
-    	if(boxx == 790) {
+    	if(boxx == getWidth()-10) {
     		moveH = -5;
     		if(moveV > 0) {
-    			moveV = 1;
+    			moveV = vy;
     		} else {
-    			moveV = -1;
+    			moveV = vy*-1;
     		}
     	}
 
     	if(boxx == 0) {
     		moveH = 5;
 			if(moveV > 0) {
-    			moveV = 1;
+    			moveV = vy;
     		} else {
-    			moveV = -1;
+    			moveV = vy*-1;
     		}
     	}
 
-    	if(boxy == 590) {
-    		moveV = -1;
+    	if(boxy == getHeight()-10) {
+    		moveV = -2;
     		if(moveH > 0) {
-    			moveH = 0-moveH;
+    			moveH = vx;
     		} else {
-    			moveH = -1*moveH;
+    			moveH = vx*-1;
     		}
     	}
 
     	if(boxy == 0) {
-    		moveV = 1;
+    		moveV = 2;
     		if(moveH > 0) {
-    			moveH = 0-moveH;
+    			moveH = vx;
     		} else {
-    			moveH = -1*moveH;
+    			moveH = vx*-1;
     		}
     	}
+    }
+}
+
+class Paddle implements KeyListener {
+	private int boxx,boxy,moveR,moveL;
+	private boolean []keys;
+
+	public Paddle() {
+		keys = new boolean[KeyEvent.KEY_LAST+1];
+		boxx = 237;
+		boxy = 620;
+		moveR = 5;
+		moveL = -5;
+		addKeyListener(this);
+	}
+
+	public void addNotify() {
+        super.addNotify();
+        setFocusable(true);
+        requestFocus();
+    }
+
+	public void move(){
+		if(keys[KeyEvent.VK_RIGHT] ){
+			boxx += moveR;
+		}
+		if(keys[KeyEvent.VK_LEFT] ){
+			boxx += moveL;
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {}
+
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
+
+    public void paint(Graphics g){
+		g.setColor(Color.white);
+		g.fillRect(boxx,boxy,75,10);
     }
 }
