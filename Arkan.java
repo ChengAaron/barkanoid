f5ece445f5504bd7dcdd549cb83a36e75cf1dfe7
Rick <richardzhou55@gmail.com>
2020-01-23 22:18:31 -0500

    w

--- Arkan.java
+++ Arkan.java
@@ -1,162 +1,235 @@
-import java.util.Scanner;
-import java.util.Random;
-import java.util.ArrayList;
-import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.*;
 import javax.swing.*;
+import java.awt.MouseInfo;
 
-public class Arkan extends JFrame{
-public class Arkan {
-	public static Level currentLevel;
-	
-	public static void main(String[] args) throws IOException{
-		Scanner inFile = new Scanner(new BufferedReader(new FileReader("levels.txt")));
-		
-		ArrayList<Level> levels = new ArrayList<Level>();
-		int numLevels = Integer.parseInt(inFile.nextLine());
-		for(int i=0;i<numLevels;i++){
-			String line = inFile.nextLine();
-			Level lev = new Level(line,i);
-			levels.add(lev);//add to Level arraylist
-			
-			System.out.print("\n");//**************************************************************************************************
-		}
-		currentLevel = levels.get(0);
-		/*while(currentLevel.getLives()>0 && levels.size()>0){
-			gameRunning();
-		}*/
-		
+public class arkan extends JFrame implements ActionListener{
+	Timer myTimer;
+	GamePanel game;
+
+    public arkan() {
+		super("Move the Ball");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setSize(800,650);
+
+		myTimer = new Timer(10, this);// trigger every 10 ms
+
+
+		game = new GamePanel(this);
+		add(game);
+		pack();
+		//setResizable(false);
+		setVisible(true);
+    }
+
+	public void start(){
+		myTimer.start();
 	}
-	public static void gameRunning(){
-		currentLevel.buildLayout();
+
+	public void actionPerformed(ActionEvent evt){
+		game.move();
+		game.repaint();
+	}
+
+    public static void main(String[] arguments) {
+	 arkan frame = new arkan();
+    }
+}
+
+class GamePanel extends JPanel implements KeyListener{
+	private boolean []keys;
+	private int moveH, moveV, ballx, bally, padx, pady;
+	private Image back, ballPic, paddlePic;
+	private arkan mainFrame;
+	private boolean moving;
+
+	Ball ball = new Ball(170, 170, 5, 1);
+	Paddle paddle = new Paddle(237, 620, 5, -5);
+
+	public GamePanel (arkan m){
+		keys = new boolean[KeyEvent.KEY_LAST+1];
+		back = new ImageIcon("background.png").getImage();
+		ballPic = new ImageIcon("ball.png").getImage();
+		paddlePic = new ImageIcon("paddle.png").getImage();
+
+		mainFrame = m;
+		moveH = ball.getVX();
+		moveV = ball.getVY();
+        ballx = ball.getBallX();
+        bally = ball.getBallY();
+        padx = paddle.getPadX();
+        pady = paddle.getPadY();
+        moving = false;
+
+
+		setPreferredSize( new Dimension(550, 650));
+        addKeyListener(this);
+	}
+
+    public void addNotify() {
+        super.addNotify();
+        setFocusable(true);
+        requestFocus();
+        mainFrame.start();
+    }
+
+	public void move(){
+		if(keys[KeyEvent.VK_UP] ){
+			moving = true;
+		}
+		if(keys[KeyEvent.VK_RIGHT] ){
+			paddle.setPadX(paddle.getMoveR());
+		}
+		if(keys[KeyEvent.VK_LEFT] ){
+			paddle.setPadX(paddle.getMoveL());
+		}
+		if(moving == true){
+			ballx += ball.getVX();
+			bally += ball.getVY();
+			collisions();
+		}
+
+
+		Point mouse = MouseInfo.getPointerInfo().getLocation();
+		Point offset = getLocationOnScreen();
+		//System.out.println("("+(mouse.x-offset.x)+", "+(mouse.y-offset.y)+")");
+		System.out.println(ballx+", "+ball.getBallX()+", "+ball.getVX());
+		//System.out.println("("+(ballx)+", "+(bally)+")");
+	}
+
+    public void keyTyped(KeyEvent e) {}
+
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
+
+    public void paint(Graphics g){
+    	g.drawImage(back,0,0,null); //background
+		g.drawImage(ballPic, ballx, bally, null);
+		g.drawImage(paddlePic, paddle.getPadX(), paddle.getPadY(), null);
+    }
+
+    public void collisions() {
+
+    	if(ballx >= getWidth()-17) {
+    		ball.setVX(-5);
+    		if(ball.getVY() > 0) {
+    			ball.setVY(-5);
+    		} else {
+    			ball.setVY(5);
+    		}
+    	}
+
+    	if(ballx <= 0) {
+    		ball.setVX(5);
+			if(moveV > 0) {
+    			moveV = ball.getVY();
+    		} else {
+    			moveV = ball.getVY()*-1;
+    		}
+    	}
+
+    	if(bally == 0) {
+    		ball.setVY(1);
+    		if(moveH > 0) {
+    			moveH = ball.getVX();
+    		} else {
+    			moveH = ball.getVX()*-1;
+    		}
+    	}
+
+    	if(bally >= getHeight()-17) {
+    		ball.setVY(-1);
+    		if(moveH > 0) {
+    			moveH = ball.getVX();
+    		} else {
+    			moveH = ball.getVX()*-1;
+    		}
+    		//gameOver();
+    	}
+    }
+
+    public void gameOver() {
+    	System.out.println("Game Over");
+    	System.exit(0);
+    }
+}
+
+
+class Ball {
+	private int ballx,bally,vx,vy;
+
+	public Ball(int ballx, int bally, int vx, int vy) {
+		this.ballx = ballx;
+		this.bally = bally;
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+	public int getBallX() {
+		return ballx;
+	}
+
+	public int getBallY() {
+		return bally;
+	}
+
+	public int getVX() {
+		return vx;
+	}
+
+	public int getVY() {
+		return vy;
+	}
+
+	public void setBallx(int ballx) { 
+		this.ballx += ballx;
+	}
+
+	public void setBally(int bally) {
+		this.bally += bally;
+	}
+
+	public void setVX(int i) {
+		vx = i;
+	}
+
+	public void setVY(int i) {
+		vx = i;
 	}
 }
 
-class Level{
-	private int lvlNum;
-	private int lives;
-	private ArrayList<Block> layout;
-	private String[] colours;
-	private int[] fillAmount;
-	
-	public Level(String line,int currentLvl){
-		layout = new ArrayList<Block>();
-		lvlNum = currentLvl;
-		lives = 5;
-		String[] rawAssets = line.split(",");
-		int numTrueObjects = rawAssets.length/2;//objects in pairs: colour and amount
-		colours = new String[numTrueObjects];
-		fillAmount = new int[numTrueObjects];
-		int count = 0;
-		for(int i=0;i<rawAssets.length;i++){
-			if(i%2==0){//Colors (String array)
-				colours[count]=rawAssets[i];
-			}
-			else{//Blocks to create(int array)
-				fillAmount[count]=Integer.parseInt(rawAssets[i]);
-				count++;//only increases every 2 indexes
-			}
-		}
-		loadLayout(numTrueObjects);
-	}
-	public void loadLayout(int numObj){
-		/////////////pseudo-grid/////////////
-		int currentPos = 0;//current position on grid
-		int line1 = 11;
-		int line2 = 22;
-		int line3 = 33;//    Ends of each line on the grid
-		int line4 = 44;
-		int line5 = 55;//    maximum level size is 5 rows
-		/////////////////////////////////////
-		
-		int x,y = 0;//*********************NOTE TO SELF************************if code is not working: try moving this inside the for loop
-		for(int i=0;i<numObj;i++){//for each colour and amount
-			for(int j=0;j<fillAmount[i];j++){//make fillAmount[i] Blocks
-				currentPos+=1;
-				if(currentPos<=line1){
-					y = 1;//actual position determined later with scalar constants
-					x = currentPos;
-					Block b = new Block(x,y,colours[i]);
-					layout.add(b);//add the block to arrayList
-				}
-				if(currentPos>line1 && currentPos<=line2){//between line1 and line2
-					y = 2;
-					x = currentPos-11;//x will be adjusted for subsequent lines
-					Block b = new Block(x,y,colours[i]);
-					layout.add(b);
-				}
-				if(currentPos>line2 && currentPos<=line3){
-					y = 3;
-					x = currentPos-22;
-					Block b = new Block(x,y,colours[i]);
-					layout.add(b);
-				}
-				if(currentPos>line3 && currentPos<=line4){
-					y = 4;
-					x = currentPos-33;
-					Block b = new Block(x,y,colours[i]);
-					layout.add(b);
-				}
-				if(currentPos>line4 && currentPos<=line5){
-					y = 5;
-					x = currentPos-44;
-					Block b = new Block(x,y,colours[i]);
-					layout.add(b);
-				}
-			}
-		}
-	}
-	public void buildLayout(){
-		System.out.println("PLACEHOLDER buildLayout");
-	}
-	
-	public int getLvl(){
-		return lvlNum;
-	}
-	public int getLives(){
-		return lives;
-	}
-}
+class Paddle {
+	private int padx,pady,moveR,moveL;
 
-class Block{
-	private int x,y,width,height;//position and dimensions
-	private int hp,colour;//intrinsic properties
-	private boolean holdsPowerUp;
-	
-	public static int SILVER,WHITE,LGREY,DGREY,BLACK;//colours for the blocks
-	
-	public Block(int rawX,int rawY,String col){
-		SILVER=0;WHITE=1;LGREY=2;DGREY=3;BLACK=4;
-		width = 50; height = 20;
-		x = rawX*width;
-		y = rawY*height+20;//shifted down 20px
-		//after multiplying and shifting, last possible block should end up at 550,120
-		hp = 1;//minimum hp
-		if(col=="Silver"){
-			colour = SILVER;
-			hp+=2;//Silver blocks take 3 hits
-		}
-		if(col=="White"){
-			colour = WHITE;
-		}
-		if(col=="LGrey"){
-			colour = LGREY;
-		}
-		if(col=="DGrey"){
-			colour = DGREY;
-		}
-		if(col=="Black"){
-			colour = BLACK;
-		}
-		holdsPowerUp = false;
-		//each block has a 1/10 chance of holding one of three power ups
-		int powerPick = (int)(Math.random() * 10 + 1);//random number between 1 and 10
-		if(powerPick==1){
-			holdsPowerUp = true;
-		}
-		
-		System.out.println(colour+x+","+y+holdsPowerUp);//**************************************************************************
+	public Paddle(int padx, int pady, int moveR, int moveL) {
+		this.padx = padx;
+		this.pady = pady;
+		this.moveR = moveR;
+		this.moveL = moveL;
+	}
+
+	public int getPadX() {
+		return padx;
+	}
+
+	public int getPadY() {
+		return pady;
+	}
+
+	public int getMoveR() {
+		return moveR;
+	}
+
+	public int getMoveL() {
+		return moveL;
+	}
+
+	public void setPadX(int move) {
+		padx += move;
 	}
 }
